2f48104c2022c72103d52439943803a55fc8a20c
nuki1048 <112338685+nuki1048@users.noreply.github.com>
2023-05-19 07:50:09 +0300

    fix

--- src/components/title/Title.jsx
+++ src/components/title/Title.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import logo from '../../assets/icons/logo.svg';
 
-const Title = () => <img src={logo} alt="Logo pomodoro " />;
+const Title = () => (
+  <img src={logo} alt="Logo pomodoro" style={{ marginTop: '48px' }} />
+);
 
 export default Title;
